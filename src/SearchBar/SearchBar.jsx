a5f4879860c909f9fe646c7f2cf13094e9885248
dkaliazin <dkaliazin@gmail.com>
2024-08-17 16:13:36 +0300

    123

---
+++ src/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { Formik, Form, Field } from "formik";
+import css from "./SearchBar.module.css";
+
+export default function SearchBar({ onSearch }) {
+  return (
+    <header>
+      <Formik
+        initialValues={{ topicName: "" }}
+        onSubmit={(values, actions) => {
+          onSearch(values.topicName);
+          actions.resetForm();
+        }}
+      >
+        <Form>
+          <Field
+            type="text"
+            name="topicName"
+            autoComplete="off"
+            placeholder="Search by name"
+          />
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+    </header>
+  );
+}
